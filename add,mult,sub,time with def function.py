2a5b81fc596e1149561e1ce747d8f621d1b7d648
Mugdass <84940755+Mugdass@users.noreply.github.com>
2021-06-03 18:13:55 -0700

    Add files via upload

---
+++ add,mult,sub,time with def function.py
@@ -0,0 +1,54 @@
+
+
+fn=input("enter first number")
+sn=input("enter second number")
+
+
+def Sum(fn, sn):
+    answer= int (fn) + int (sn)
+    return answer
+
+answer= Sum(fn,sn)
+print(int(answer))
+
+
+
+
+
+
+
+def Diff(fn, sn):
+    answer1= int (fn) - int (sn)
+    return answer1
+
+answer1= Diff(fn,sn)
+print(int(answer1))
+
+
+
+
+
+
+
+def Mult(fn, sn):
+    answer3= int (fn) * int (sn)
+    return answer3
+
+answer3= Mult(fn,sn)
+print(int(answer3))
+
+
+
+
+
+
+
+def Div(fn, sn):
+    answer4= int (fn) / int (sn)
+    return answer4
+
+answer4= Div(fn,sn)
+print(int(answer4))
+
+
+
